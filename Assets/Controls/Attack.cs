7adbbfc797424b1578b421d6e6e81dcd724e4154
Pelicinius <Michael.zyznik@outlook.com>
2014-09-13 00:56:30 +1000

    Shooting
    Creates bullets, don't move yet

--- Assets/Controls/Attack.cs
+++ Assets/Controls/Attack.cs
@@ -3,13 +3,22 @@ using System.Collections;
 
 public class Attack : MonoBehaviour {
 
+	public GameObject projectile;
+	public Transform front;
+	public float speed;
 	// Use this for initialization
 	void Start () {
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update()
+	{
+		if (Input.GetMouseButtonDown (0)) 
+		{
+			Debug.Log ("Pressed left click.");
+			Instantiate (projectile, front.position, front.rotation);
+
+		}
 	}
 }
